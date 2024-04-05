26c600fa58bfac28f24689f7d9ecd3dbf518c7bc
simpleton rabs <74567810+simpleton-rabs@users.noreply.github.com>
2024-04-05 10:57:19 +0100

    fttf
    tfft

--- js/sketch.js
+++ js/sketch.js
@@ -1,36 +1,42 @@
 //values for rectangles on left side
-let recWidth = 0;
-let recHeight = 0;
-const recHeightDiv = 15;
-const recWidthDiv = 9;
+let recWidth = 200;
+let recHeight = 100;
+// const recHeightDiv = 15;
+// const recWidthDiv = 9;
 let recX = 5;
 let recY = 5;
 
-// quadXOne = recX + recWidth;
-// let quadXOne = 5;
-// let quadYOne = 5;
-// let quadXTwo = quadXOne + 100;
-// let quadYTwo = quadYOne + 100;
-// quadXTwo = quadXOne + (2 * recWidth);
-// quadYTwo = quadYOne - recWidth;
+  // values for quadrangles coming off left rectangles
 
-
-// let quadXThree = quadXTwo;
-// let quadYThree = quadYTwo + 100;
-// let quadXFour = quadXOne; 
-// // quadYFour = quadYOne + recHeight;
-// let quadYFour = quadYOne + recHeight;
-
+  // let quadXOne = recX + recWidth;
+  let quadXOne = 205;
+  // let quadYOne = recY;
+  let quadYOne = 5;
+  // let quadXTwo = quadXOne + 100;
+  let quadXTwo = quadXOne + (2 * recWidth);
+  // let quadYTwo = quadYOne + 100;
+  let quadYTwo = quadYOne - recHeight;
+  
+  // quadXTwo = quadXOne + (2 * recWidth);
+  // quadYTwo = quadYOne - recWidth;
+  
+  
+  let quadXThree = quadXTwo;
+  // let quadYThree = quadYTwo + 100;
+  let quadYThree = quadYOne;
+  let quadXFour = quadXOne;
+  let quadYFour = quadYOne + recHeight;
+  // let quadYFour = quadYOne + recHeight;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(220);
   //have to set here after canvas is declared
-  recWidth = width / recWidthDiv;
-  recHeight = height / recHeightDiv;
-  
-  // values for quadrangles coming off left rectangles
+  // recWidth = width / recWidthDiv;
+  // recHeight = height / recHeightDiv;
 }
+
+
 
 function draw() {
   // background(220);
@@ -39,19 +45,30 @@ function draw() {
   // noStroke();
   
   while (recY < height){
-  rect(recX, recY, recWidth, recHeight, 5);
+   rect(recX, recY, recWidth, recHeight, 5);
       // quad(quadXOne, quadYOne, quadXTwo, quadYTwo, quadXThree, quadYThree, quadXFour, quadYFour);
     
     recY += recHeight;
-    
+    // quadYOne += recHeight;
     
     // quadXOne += recHeight;
     // quadYOne += recHeight; 
     
       }
-  
-    //   quad(quadXOne, quadYOne, quadXTwo, quadYTwo, quadXThree, quadYThree, quadXFour, quadYFour);
 
+      while (quadYOne < height){
+      quad(quadXOne, quadYOne, quadXTwo, quadYTwo, quadXThree, quadYThree, quadXFour, quadYFour);
+      quadYOne += recHeight;
+      quadYTwo += recHeight;
+      quadYThree += recHeight;
+      QuadYFour += recHeight;
+      }     
+     
+      // quadYOne + recHeight;
+      // quadYTwo = quadYOne - recHeight;
+      // quadYThree = quadYOne;
+      // quadYFour = quadYOne + recHeight;
+      // quad(quadXOne, quadYOne, quadXTwo, quadYTwo, quadXThree, quadYThree, quadXFour, quadYFour);
   // strokeWeight(50);
   
   
